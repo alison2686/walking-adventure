3fa03338a6ef01d9c9d184c5f548ce4d492a26ef
Alison Lee <alison2686@gmail.com>
2023-02-13 19:13:39 -0800

    fix navbar on home page

--- src/App.js
+++ src/App.js
@@ -1,44 +1,20 @@
-// import axios from 'axios';
-// import Points from './components/Points';
-// import { useState } from 'react';
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import About from "./pages/About";
-// import ResultsForm from './Components/ResultsForm';
-import Results from './pages/Results';
-
-// const API_URL = "https://adventure.mocklab.io/api/v1/points";
-
-// function getAPIData() {
-//     return axios.get(API_URL).then((response) => response.data)
-// }
+import Results from "./pages/Results";
 
 function App() {
-    // const [points, setPoints] = useState([]);
-
-    // useEffect(() => {
-    //     let mounted = true;
-    //     getAPIData().then((items) => {
-    //         if (mounted) {
-    //             setPoints(items);
-    //         }
-    //     });
-    //     return () => (mounted = false);
-    // }, []);
-
-    return (
-    <div >
-    <BrowserRouter>
+  return (
+    <div>
+      <BrowserRouter>
         <Routes>
-          <Route path ="/" element={<LandingPage/>}/>
-          <Route path="/home" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          {/* <Route path="/results" element={<ResultsForm search="Seattle, Washington" miles={2}/>}/> */}
-          <Route path='/results' element={<Results/>}/>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/results" element={<Results />} />
         </Routes>
       </BrowserRouter>
-        {/* < Points points={points} /> */}
     </div>
   );
 }
--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,171 +1,140 @@
-import React, { useState, useEffect } from 'react'
-import Map, {GeolocateControl, Marker, NavigationControl, Popup } from 'react-map-gl'
-import 'mapbox-gl/dist/mapbox-gl.css'
-// import axios from 'axios'
-import locationData from '../../Utils/mapdata.json'
-// import point from '../../assets/map-img/map-pin.svg'
-import MapSearch from '../MapSearch/MapSearch'
-import {IoMdPhotos} from 'react-icons/io'
-import '../../index.css'
-import DeckGL from '@deck.gl/react';
-import {LineLayer} from '@deck.gl/layers';
+import React, { useState, useEffect } from "react";
+import Map, {
+  GeolocateControl,
+  Marker,
+  NavigationControl,
+  Popup,
+} from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import locationData from "../../Utils/mapdata.json";
+import MapSearch from "../MapSearch/MapSearch";
+import { IoMdPhotos } from "react-icons/io";
+import "../../index.css";
+import DeckGL from "@deck.gl/react";
+import { LineLayer } from "@deck.gl/layers";
 
 const INITIAL_VIEW_STATE = {
-    longitude: -122.43758758255566,
-    latitude: 37.79120588317128,
-    zoom: 13,
-    pitch: 0,
-    bearing: 0
-  };
+  longitude: -122.43758758255566,
+  latitude: 37.79120588317128,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0,
+};
 
 const data = [
-    {
-        sourcePosition: [-122.41858479929094, 37.76367466576563],
-        targetPosition: [-122.40364923013956, 37.76068122656553]
-    }
-]
- 
+  {
+    sourcePosition: [-122.41858479929094, 37.76367466576563],
+    targetPosition: [-122.40364923013956, 37.76068122656553],
+  },
+];
 
 const Maps = () => {
-    const layers = [
-        new LineLayer({id: 'line-layer', data })
-    ]
+  const layers = [new LineLayer({ id: "line-layer", data })];
 
-    const [viewport, setViewport] = useState({
-        latitude: 37.79120588317128, 
-        longitude: -122.43758758255566,
-        zoom: 12,
-        width: '100vw',
-        height: '100vh'
-    })
+  const [viewport, setViewport] = useState({
+    latitude: 37.79120588317128,
+    longitude: -122.43758758255566,
+    zoom: 12,
+    width: "100vw",
+    height: "100vh",
+  });
 
-    // const [location, setLocation] = useState([])
-    const [selectedPt, setSelectedPt] = useState(null)
-    // const [showPopup, setShowPopup] = useState(true)
+  const [selectedPt, setSelectedPt] = useState(null);
 
-    useEffect(() => {
-        const listener = (e)=> {
-            if (e.key === 'Escape') {
-                setSelectedPt(null)
-            }
-        }
-        window.addEventListener('keydown', listener)
+  useEffect(() => {
+    const listener = (e) => {
+      if (e.key === "Escape") {
+        setSelectedPt(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
 
-        return () => {
-            window.removeEventListener('keydown', listener)
-        }
-    }, [])
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
 
-
-    // console.log(viewport)
-
-    // useEffect(() => {
-    //     const getData = async () => {
-    //         try{
-    //             const data = await axios.get('https://adventure.mocklab.io/api/v1/points')
-    //             setLocation(data.data)
-    //             // console.log(data)
-    //         } catch (error) {
-    //             console.log(error)
-    //         }
-    //     }
-    //     getData()
-    // }, [])
   return (
     <div>
-        <div>
-            <DeckGL
-                initalViewState={INITIAL_VIEW_STATE}
-                controller={true}
-                layers={layers}
-            ></DeckGL>
-            <div 
-            className='mapboxgl-map w-[100%] h-[80vh] bg-white border-solid border-2 md:h-[82vh] lg:w-[100%] '>
-                <Map 
-                    {...viewport} 
-                    mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-                    onMove={e => setViewport(e.viewport)}
-                    mapStyle="mapbox://styles/mapbox/streets-v11"
-                    
+      <div className="mapboxgl-map w-[100%] h-[80vh] bg-white border-solid border-2 md:h-[82vh] lg:w-[100%] ">
+        <DeckGL
+          initalViewState={INITIAL_VIEW_STATE}
+          controller={true}
+          layers={layers}
+        ></DeckGL>
+        <div className="w-[100%] h-[80vh] md:h-[82vh] lg:w-[100%] ">
+          <Map
+            {...viewport}
+            mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            onMove={(e) => setViewport(e.viewport)}
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+          >
+            {/* when uing api change locationData to location */}
+            {locationData.features.map((location) => (
+              <Marker
+                key={location.properties.id}
+                latitude={location.geometry.coordinates[1]}
+                longitude={location.geometry.coordinates[0]}
+              >
+                <button
+                  className="bg-transparent border-none cursor-pointer"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setSelectedPt(location);
+                    console.log(selectedPt);
+                  }}
                 >
-                    {/* when uing api change locationData to location */}
-                    {locationData.features.map(location => (
-                        <Marker
-                            key={location.properties.id}
-                            latitude={location.geometry.coordinates[1]}
-                            longitude={location.geometry.coordinates[0]}
-                        >
-                            <button 
-                                className='bg-transparent border-none cursor-pointer'
-                                onClick={(e) => {
-                                    e.preventDefault()
-                                    setSelectedPt(location)
-                                    console.log(selectedPt)
-                                }}
-                            >
-                            <IoMdPhotos 
-                            className='text-3xl text-white border-solid border-[#e0b94bfb] border-2 bg-[#e9ae0cfd] p-1 rounded-full shadow-lg'/>
-                            </button>
-                        </Marker>
-                    ))}
+                  <IoMdPhotos className="text-3xl text-white border-solid border-[#e0b94bfb] border-2 bg-[#e9ae0cfd] p-1 rounded-full shadow-lg" />
+                </button>
+              </Marker>
+            ))}
 
-                    {/* {locationData.features.map((location) => console.log(location.geometry.coordinates[1]))} */}
-                                                {/* location, index */}
+            {/* {locationData.features.map((location) => console.log(location.geometry.coordinates[1]))} */}
+            {/* location, index */}
 
-                    {selectedPt ? (
-                        <Popup
-                            latitude={selectedPt.geometry.coordinates[1]}
-                            longitude={selectedPt.geometry.coordinates[0]}
-                            closeButton={true}
-                            closeOnClick={false} 
-                            closeOnMove={false}
-                            onClose={() => setSelectedPt(null)}
-
-                            
-                        >
-                            <div className='mapboxgl-popup-content flex items-center justify-start w-[40vh] h-[25vh] rounded-lg'>
-                               
-                                    <div className='w-[100%] rounded-lg'>
-                                    <img 
-                                    className='p-2 w-[60%] h-[50%] rounded-md object-cover'
-                                        src={selectedPt.properties.image_1} 
-                                         alt="icon"/>
-                                </div>
-                                <div className='w-[100%]'>
-                                    <h2 className='font-poppins text-lg font-bold py-2'>{selectedPt.properties.name}</h2>
-                                    <p className='font-poppins text-md'>{selectedPt.properties.description}</p>
-                                        
-                                </div>
-                                
-                                
-                                
-                                
-                            </div>
-                        </Popup>
-                    ) : null}
-                   
-                        <NavigationControl 
-                    position='bottom-right'
-                    
+            {selectedPt ? (
+              <Popup
+                latitude={selectedPt.geometry.coordinates[1]}
+                longitude={selectedPt.geometry.coordinates[0]}
+                closeButton={true}
+                closeOnClick={false}
+                closeOnMove={false}
+                onClose={() => setSelectedPt(null)}
+              >
+                <div className="mapboxgl-popup-content flex items-center justify-start w-[40vh] h-[25vh] rounded-lg">
+                  <div className="w-[100%] rounded-lg">
+                    <img
+                      className="p-2 w-[60%] h-[50%] rounded-md object-cover"
+                      src={selectedPt.properties.image_1}
+                      alt="icon"
                     />
-                   
-                    <GeolocateControl
-                        position='top-left'
-                        trackUserLocation
-                        onGeolocate={e => setViewport(e.viewport)} 
-                       
-                    />
-                           
-                        <MapSearch 
-                        />
-                           
-                </Map>
-            </div>
-         </div>
-     </div>
-  )
-}
+                  </div>
+                  <div className="w-[100%]">
+                    <h2 className="font-poppins text-lg font-bold py-2">
+                      {selectedPt.properties.name}
+                    </h2>
+                    <p className="font-poppins text-md">
+                      {selectedPt.properties.description}
+                    </p>
+                  </div>
+                </div>
+              </Popup>
+            ) : null}
 
+            <NavigationControl position="bottom-right" />
 
+            <GeolocateControl
+              position="top-left"
+              trackUserLocation
+              onGeolocate={(e) => setViewport(e.viewport)}
+            />
 
-export default Maps
+            <MapSearch />
+          </Map>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Maps;
