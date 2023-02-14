ff1d33331d555f5fa7ec86f501f4e6668647b7a2
Alison Lee <65731037+alison2686@users.noreply.github.com>
2023-02-13 19:15:39 -0800

    Fix navigation bar (#5)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,27 @@
-import React from 'react'
-import Footer from '../Components/Footer'
-import NavBar from '../Components/NavBar'
-import InputForm from '../Components/InputForm'
-import Maps from '../Components/Map/Map'
-
+import React from "react";
+import Footer from "../Components/Footer";
+import NavBar from "../Components/NavBar";
+import InputForm from "../Components/InputForm";
+import Maps from "../Components/Map/Map";
 
 function Home() {
   return (
-    <div className='bg-white text-black h-auto overflow-x-hidden'>
-      <NavBar/>
-       {/* <div className='h-[8vh] md:flex  bg-[#69a1ac]'> </div> */}
-{/* MAP and INPUT container  */}
-    <div className='flex flex-col items-start justify-center md:flex-row bg-white h-[100%]'>
-{/* Input container */}
-           <div className='hidden bg-[#b7dce0] bg-opacity-60 md:flex  md:h-[82vh] md:w-[65%] lg:w-[25%]'>
-            <InputForm/>
-             
-           </div>
-{/* MAP container  */}
-            <div className=' w-[100%] h-[40%] bg-white border-solid border-2 md:h-[50%] lg:w-[75%] '>
-              <Maps />
-            </div>
-            
+    <div className="bg-white text-black h-auto overflow-x-hidden">
+      <NavBar />
+      <div className="flex flex-col items-start justify-center md:flex-row bg-white h-[100%]">
+        {/* Input container */}
+        <div className="hidden bg-[#b7dce0] bg-opacity-60 md:flex  md:h-[82vh] md:w-[65%] lg:w-[25%]">
+          <InputForm />
         </div>
-{/* Footer  */}
-        <Footer/>
-       
+        {/* MAP container  */}
+        <div className=" w-[100%] h-[40%] bg-white border-solid border-2 md:h-[50%] lg:w-[75%] ">
+          <Maps />
+        </div>
+      </div>
+      {/* Footer  */}
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,32 +1,29 @@
-import React from 'react'
-import Footer from '../Components/Footer'
-import NavBar from '../Components/NavBar'
-//import Map from '../components/Map/Map'
-import ResultsForm from '../Components/ResultsForm'
+import React from "react";
+import Footer from "../Components/Footer";
+import NavBar from "../Components/NavBar";
+import ResultsForm from "../Components/ResultsForm";
 
 function Results() {
   return (
     <div>
-       <div className='bg-white text-black h-full '>
-      <NavBar/>
-{/* MAP and RESULTS container  */}
-    <div className='flex flex-col items-start justify-center md:flex-row bg-white h-[100%]'>
-{/* RESULTS container */}
-           <div className='hidden bg-[#b7dce0] bg-opacity-60 md:flex  md:h-[82vh] md:w-[45%] lg:w-[25%]'>
-             <ResultsForm/>
-           </div>
-{/* MAP container  */}
-            <div className=' w-[100%] h-[83vh] bg-white border-solid border-2 md:h-[82vh] lg:w-[75%] '>
-              {/*<Map />*/}
-            </div>
-            
+      <div className="bg-white text-black h-full ">
+        <NavBar />
+        {/* MAP and RESULTS container  */}
+        <div className="flex flex-col items-start justify-center md:flex-row bg-white h-[100%]">
+          {/* RESULTS container */}
+          <div className="hidden bg-[#b7dce0] bg-opacity-60 md:flex  md:h-[82vh] md:w-[45%] lg:w-[25%]">
+            <ResultsForm />
+          </div>
+          {/* MAP container  */}
+          <div className=" w-[100%] h-[83vh] bg-white border-solid border-2 md:h-[82vh] lg:w-[75%] ">
+            {/*<Map />*/}
+          </div>
         </div>
-{/* Footer  */}
-        <Footer/>
-       
+        {/* Footer  */}
+        <Footer />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Results
+export default Results;
