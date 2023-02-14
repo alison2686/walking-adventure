f0962af2dca89eb978486bd2a01267f92f4e79f2
Alison Lee <alison2686@gmail.com>
2023-02-13 21:19:58 -0800

    update signin page

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Footer from "../Components/Footer";
-import NavBar from "../Components/NavBar";
-import naturePhone from "../assets/eg-img/nature-phone.png";
+// import Footer from "../Components/Footer";
+// import NavBar from "../Components/NavBar";
+// import naturePhone from "../assets/eg-img/nature-phone.png";
 
 const Signin = () => {
   return <div>Signin</div>;
