dcb764076ce193d0015d4b6f4e8a18f6b5d56d4a
Alison Lee <alison2686@gmail.com>
2023-02-13 21:15:56 -0800

    update navbar

---
+++ src/pages/Signin.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Footer from "../Components/Footer";
+import NavBar from "../Components/NavBar";
+import naturePhone from "../assets/eg-img/nature-phone.png";
+
+const Signin = () => {
+  return <div>Signin</div>;
+};
+
+export default Signin;
