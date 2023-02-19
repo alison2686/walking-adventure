f54a37337448d16a6731c9ce122e790cd2056bb8
Alison Lee <alison2686@gmail.com>
2023-02-19 15:39:30 -0800

    add selectedPt

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,146 +1,112 @@
-import React, { useState, useEffect } from 'react'
-import Map, {GeolocateControl, Marker, NavigationControl, Popup } from 'react-map-gl'
-import 'mapbox-gl/dist/mapbox-gl.css'
-// import axios from 'axios'
-import locationData from '../../Utils/mapdata.json'
-// import point from '../../assets/map-img/map-pin.svg'
-import MapSearch from '../MapSearch/MapSearch'
-import {IoMdPhotos} from 'react-icons/io'
-import '../../index.css'
+import React, { useState, useEffect } from 'react';
+import Map, {
+  GeolocateControl,
+  Marker,
+  NavigationControl,
+  Popup,
+} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import locationData from '../../Utils/mapdata.json';
+import MapSearch from '../MapSearch/MapSearch';
+import { IoMdPhotos } from 'react-icons/io';
+import '../../index.css';
 
 const Maps = () => {
-    const [viewport, setViewport] = useState({
-        latitude: 37.79120588317128, 
-        longitude: -122.43758758255566,
-        zoom: 12,
-        width: '100vw',
-        height: '100vh'
-    })
+  const [viewport, setViewport] = useState({
+    latitude: 37.79120588317128,
+    longitude: -122.43758758255566,
+    zoom: 12,
+    width: '100vw',
+    height: '100vh',
+  });
 
-     
+  const [selectedPt, setSelectedPt] = useState(null);
 
-    // const [location, setLocation] = useState([])
-    const [selectedPt, setSelectedPt] = useState(null)
-    // const [showPopup, setShowPopup] = useState(true)
+  useEffect(() => {
+    const listener = (e) => {
+      if (e.key === 'Escape') {
+        setSelectedPt(null);
+      }
+    };
+    window.addEventListener('keydown', listener);
 
-    useEffect(() => {
-        const listener = (e)=> {
-            if (e.key === 'Escape') {
-                setSelectedPt(null)
-            }
-        }
-        window.addEventListener('keydown', listener)
+    return () => {
+      window.removeEventListener('keydown', listener);
+    };
+  }, []);
 
-        return () => {
-            window.removeEventListener('keydown', listener)
-        }
-    }, [])
-
-
-    // console.log(viewport)
-
-    // useEffect(() => {
-    //     const getData = async () => {
-    //         try{
-    //             const data = await axios.get('https://adventure.mocklab.io/api/v1/points')
-    //             setLocation(data.data)
-    //             // console.log(data)
-    //         } catch (error) {
-    //             console.log(error)
-    //         }
-    //     }
-    //     getData()
-    // }, [])
   return (
     <div>
-        <div>
-            <div 
-            className='mapboxgl-map w-[100%] h-[80vh] bg-white border-solid border-2 md:h-[82vh] lg:w-[100%] '>
-                <Map 
-                    {...viewport} 
-                    mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-                    onMove={e => setViewport(e.viewport)}
-                    mapStyle="mapbox://styles/mapbox/streets-v11"
-                    
+      <div>
+        <div className='mapboxgl-map w-[100%] h-[80vh] bg-white border-solid border-2 md:h-[82vh] lg:w-[100%] '>
+          <Map
+            {...viewport}
+            mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            onMove={(e) => setViewport(e.viewport)}
+            mapStyle='mapbox://styles/mapbox/streets-v11'
+          >
+            {/* when uing api change locationData to location */}
+            {locationData.features.map((location) => (
+              <Marker
+                key={location.properties.id}
+                latitude={location.geometry.coordinates[1]}
+                longitude={location.geometry.coordinates[0]}
+              >
+                <button
+                  className='bg-transparent border-none cursor-pointer'
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setSelectedPt(location);
+                    console.log(selectedPt);
+                  }}
                 >
-                    {/* when uing api change locationData to location */}
-                    {locationData.features.map(location => (
-                        <Marker
-                            key={location.properties.id}
-                            latitude={location.geometry.coordinates[1]}
-                            longitude={location.geometry.coordinates[0]}
-                        >
-                            <button 
-                                className='bg-transparent border-none cursor-pointer'
-                                onClick={(e) => {
-                                    e.preventDefault()
-                                    setSelectedPt(location)
-                                    console.log(selectedPt)
-                                }}
-                            >
-                            <IoMdPhotos 
-                            className='text-3xl text-white border-solid border-[#e0b94bfb] border-2 bg-[#e9ae0cfd] p-1 rounded-full shadow-lg'/>
-                            </button>
-                        </Marker>
-                    ))}
+                  <IoMdPhotos className='text-3xl text-white border-solid border-[#e0b94bfb] border-2 bg-[#e9ae0cfd] p-1 rounded-full shadow-lg' />
+                </button>
+              </Marker>
+            ))}
 
-                    {/* {locationData.features.map((location) => console.log(location.geometry.coordinates[1]))} */}
-                                                {/* location, index */}
-
-                    {selectedPt ? (
-                        <Popup
-                            latitude={selectedPt.geometry.coordinates[1]}
-                            longitude={selectedPt.geometry.coordinates[0]}
-                            closeButton={true}
-                            closeOnClick={false} 
-                            closeOnMove={false}
-                            onClose={() => setSelectedPt(null)}
-
-                            
-                        >
-                            <div className='mapboxgl-popup-content flex items-center justify-start w-[40vh] h-[25vh] rounded-lg'>
-                               
-                                    <div className='w-[100%] rounded-lg'>
-                                    <img 
-                                    className='p-2 w-[60%] h-[50%] rounded-md object-cover'
-                                        src={selectedPt.properties.image_1} 
-                                         alt="icon"/>
-                                </div>
-                                <div className='w-[100%]'>
-                                    <h2 className='font-poppins text-lg font-bold py-2'>{selectedPt.properties.name}</h2>
-                                    <p className='font-poppins text-md'>{selectedPt.properties.description}</p>
-                                        
-                                </div>
-                                
-                                
-                                
-                                
-                            </div>
-                        </Popup>
-                    ) : null}
-                   
-                        <NavigationControl 
-                    position='bottom-right'
-                    
+            {selectedPt ? (
+              <Popup
+                latitude={selectedPt.geometry.coordinates[1]}
+                longitude={selectedPt.geometry.coordinates[0]}
+                closeButton={true}
+                closeOnClick={false}
+                closeOnMove={false}
+                onClose={() => setSelectedPt(null)}
+              >
+                <div className='mapboxgl-popup-content flex items-center justify-start w-[40vh] h-[25vh] rounded-lg'>
+                  <div className='w-[100%] rounded-lg'>
+                    <img
+                      className='p-2 w-[60%] h-[50%] rounded-md object-cover'
+                      src={selectedPt.properties.image_1}
+                      alt='icon'
                     />
-                   
-                    <GeolocateControl
-                        position='top-left'
-                        trackUserLocation
-                        onGeolocate={e => setViewport(e.viewport)} 
-                       
-                    />
-                           
-                        <MapSearch 
-                        />
-                           
-                </Map>
-            </div>
-         </div>
-     </div>
-  )
-}
+                  </div>
+                  <div className='w-[100%]'>
+                    <h2 className='font-poppins text-lg font-bold py-2'>
+                      {selectedPt.properties.name}
+                    </h2>
+                    <p className='font-poppins text-md'>
+                      {selectedPt.properties.description}
+                    </p>
+                  </div>
+                </div>
+              </Popup>
+            ) : null}
 
+            <NavigationControl position='bottom-right' />
 
+            <GeolocateControl
+              position='top-left'
+              trackUserLocation
+              onGeolocate={(e) => setViewport(e.viewport)}
+            />
+            <MapSearch />
+          </Map>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Maps
+export default Maps;
